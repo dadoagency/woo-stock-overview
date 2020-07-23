5e8ec2ca79c3ee72082848369f4229a1558ec95c
Max <maxtyrrell0@gmail.com>
2020-07-23 15:29:58 +0200

    Refactor

--- index.php
+++ index.php
@@ -1,127 +1,149 @@
-add_filter( 'manage_edit-product_columns', 'customise_columns' );
-function customise_columns( $columns ){
-	$new_columns = ( is_array( $columns ) ) ? $columns : array();
-	
-	//Remove columns we don't need
-  	unset( $new_columns[ 'sku' ] );
-	unset($new_columns['product_tag']);
-	unset($new_columns['featured']);
-	unset($new_columns['date']);
-		unset($new_columns['is_in_stock']);
+<?php
 
+/**
+ * Plugin Name: WooCommerce Variation Stock
+ * Description: See stock on a per-variation level on the admin dashboard Products page without having to click into the product.
+ * Version: 1.0.0
+ * License: MIT
+ * Author: Dado
+ * Author URI: https://dadoagency.com
+ * Requires PHP: 5.3.0
+ */
 
-	//Add an empty column for variation level stock
-	return array_slice( $new_columns, 0, 3, true )
-	+ array( 'var_stock' => 'Variation Stock' )
-	+ array( 'var_stock_summary' => 'Stock' )
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
-	+ array_slice( $new_columns, 3, NULL, true );
+// Unset the default stock column and add our own
+add_filter('manage_edit-product_columns', 'dado_customise_columns');
+function dado_customise_columns($columns) {
+  $new_columns = (is_array($columns)) ? $columns : array();
 
+  // Remove the default stock column
+  unset($new_columns['is_in_stock']);
+
+  // Add our two new stock columns.
+  // We slice the columns so we can stick our variation
+  // columns in a convenient position (near where the default Stock column sits)
+  return array_slice($new_columns, 0, 3, true)
+    + array('var_stock' => 'Variation Stock')
+    + array('var_stock_summary' => 'Stock')
+    + array_slice($new_columns, 3, NULL, true);
 }
 
+// Populate the new stock columns
+add_action('manage_posts_custom_column', 'dado_populate_variation_stock');
+function dado_populate_variation_stock($column_name) {
+  // TODO: Make this configurable
+  $LOW_STOCK_THRESHOLD = 4;
 
+  // Populate the 'var_stock' column
+  if ($column_name == 'var_stock') {
+    $productId = get_the_ID();
+    $product   = wc_get_product($productId);
 
-add_action( 'manage_posts_custom_column', 'dado_populate_variation_stock' );
-function dado_populate_variation_stock( $column_name ) {
- 
-	if( $column_name  == 'var_stock' ) {
-		$productId =  get_the_ID(); // taxonomy name
-		$product = wc_get_product($productId);
-		
-		 if ( $product->get_type() == 'variable' ) {
-        foreach ( $product->get_available_variations() as $key ) {
-            $attr_string = array();
-            foreach ( $key['attributes'] as $attr_name=> $attr_title ) {
-				$attr_title_formatted = str_replace('-',' ',$attr_title);
+    if ($product->get_type() == 'variable') {
+      foreach ($product->get_children() as $variation_id) {
+        $variation = new WC_Product_Variation($variation_id);
 
-                $attr_string[] = $attr_title_formatted;
-            }
-            if ( $key['max_qty'] < 4 and $key['max_qty'] >0) { 
-               echo '<div style="display:flex; flex-wrap:nowrap; align-items:center;"> <div style="border-radius:50px; width:10px; height:10px; background-color:orange; margin-right:5px;"></div>' . implode( ', ', $attr_string ) . ': ' . $key['max_qty'] . ' In Stock</div>'; 
-            } 
-			
-			elseif($key['max_qty']<=0){
-              echo '<div style="display:flex; flex-wrap:nowrap; align-items:center;"><div style="border-radius:50px; width:10px; height:10px; background-color:red; margin-right:5px;"></div>' . implode(', ', $attr_string ) . ': Out Of Stock</div>'; 
+        $variation_name = implode(", ", str_replace('-', ' ', $variation->get_attributes()));
 
-			}
-			else { 
-              echo '<div style="display:flex; flex-wrap:nowrap; align-items:center;"> <div style="border-radius:50px; width:10px; height:10px; background-color:green; margin-right:5px;"></div>' . implode( ', ', $attr_string ) . ': ' . $key['max_qty'] . ' In Stock</div>'; 
-            }
+        $stock = $variation->get_stock_quantity();
+
+        if ($stock < $LOW_STOCK_THRESHOLD and $stock > 0) {
+          // Low stock
+          echo get_stock_detail_html('orange', $variation_name . ': ' . $stock . ' in stock');
+        } elseif ($stock == 0) {
+          // Out of stock
+          echo get_stock_detail_html('red', $variation_name . ': out of stock');
+        } elseif ($stock < 0) {
+          // On backorder
+          echo get_stock_detail_html('purple', $variation_name . ': ' . abs( $stock ) . ' on backorder');
+        } else {
+          // In stock
+          echo get_stock_detail_html('green', $variation_name . ': ' . $stock . ' in stock');
         }
+      }
     }
-		//Assumed Single product
-		else{
-		$stock = $product->get_stock_quantity();
-		if ( $stock < 4 and $stock >0) { 
-               echo '<div style="display:flex; flex-wrap:nowrap; align-items:center;"> <div style="border-radius:50px; width:10px; height:10px; background-color:orange; margin-right:5px;"></div> ' . $stock . ' In Stock</div>'; 
-            } 
-			
-			elseif($stock<=0){
-              echo '<div style="display:flex; flex-wrap:nowrap; align-items:center;"><div style="border-radius:50px; width:10px; height:10px; background-color:red; margin-right:5px;"></div> Out Of Stock</div>'; 
 
-			}
-			else { 
-              echo '<div style="display:flex; flex-wrap:nowrap; align-items:center;"> <div style="border-radius:50px; width:10px; height:10px; background-color:green; margin-right:5px;"></div> ' . $stock . ' In Stock</div>'; 
-            }
-			
-		}
-		
-		echo $x;
-	}
-	
-	if( $column_name  == 'var_stock_summary' ) {
-		$productId =  get_the_ID(); // taxonomy name
-		$product = wc_get_product($productId);
-		
-		 if ( $product->get_type() == 'variable' ) {
-			 $numberLowStock=0;
-			 $numberOutOfStock=0;
+    // TODO: Make sure this works for grouped, bundled, etc products
+    else {
+      $stock = $product->get_stock_quantity();
+      if ($stock < $LOW_STOCK_THRESHOLD and $stock > 0) {
+        // Low stock
+        echo get_stock_detail_html('orange', $stock . ' in stock');
+      } elseif ($stock == 0) {
+        // Out of stock
+        echo get_stock_detail_html('red', 'Out of stock');
+      } elseif ($stock < 0) {
+        // On backorder
+        echo get_stock_detail_html('purple', abs( $stock ) . ' on backorder');
+      } else {
+        // In stock
+        echo get_stock_detail_html('green', $stock . ' in stock');
+      }
+    }
+  }
 
-        foreach ( $product->get_available_variations() as $key ) {
-            $attr_string = array();
-            foreach ( $key['attributes'] as $attr_name => $attr_value ) {
-                $attr_string[] = $attr_value;
-            }
-            if ( $key['max_qty'] < 4 and $key['max_qty'] >0) { 
-		$numberLowStock+=1;
-            } 
-			
-			if($key['max_qty']==0){
-				$numberOutOfStock+=1;
-			}
+  // Populate the 'var_stock_summary' column
+  if ($column_name == 'var_stock_summary') {
+    $productId = get_the_ID();
+    $product   = wc_get_product($productId);
 
+    if ($product->get_type() == 'variable') {
+      $number_low_stock    = 0;
+      $number_out_of_stock = 0;
+      $number_backorder    = 0;
+
+      // Count the number of low stock and out of stock variations
+      foreach ($product->get_children() as $variation_id) {
+        $variation = new WC_Product_Variation($variation_id);
+        $variation_stock = $variation->get_stock_quantity();
+
+        if ($variation_stock < $LOW_STOCK_THRESHOLD and $variation_stock > 0) {
+          $number_low_stock += 1;
+        } elseif ($variation_stock == 0) {
+          $number_out_of_stock += 1;
+        } elseif ($variation_stock < 0) {
+          $number_backorder += 1;
         }
-			 if($numberLowStock>0 and $numberOutOfStock==0){
-				 $variation_or_variations="Variations";
-				 if($numberLowStock==1){$variation_or_variations="Variation";}
-				 echo '<div style="color:orange;">Low Stock | '.$numberLowStock.' '.$variation_or_variations.'</div>';
-			 }
-			 
-			 elseif($numberLowStock==0 and $numberOutOfStock==0){
-				 echo '<div style="color:green;">In Stock | All Variations </div>';
-			 }
-			 
-			 else{
-				 echo '<div style="color:red;">Out Of Stock | '.$numberOutOfStock.' Variations</div>';
+      }
 
-			 }
-			 echo $numberLowOrOutOfStock;
+      // Stock summary priorities:
+      // If there are any products on backorder, that is the most important thing to display, so we choose to display that.
+      // If there are no products on backorder, but there are products out of stock, then we display that.
+      // If there are neither backorder nor out of stock, but there are products low in stock, then we display that.
+      // Otherwise, everything must be in stock.
+
+      if ($number_backorder > 0) {
+        echo get_stock_summary_html('purple', 'On backorder | ' . $number_backorder . ($number_backorder == 1 ? ' variation' : ' variations'));
+      } elseif ($number_out_of_stock > 0) {
+        echo get_stock_summary_html('red', 'Out of stock | ' . $number_out_of_stock . ($number_out_of_stock == 1 ? ' variation' : ' variations'));
+      } elseif ($number_low_stock > 0) {
+        echo get_stock_summary_html('orange', 'Low stock | ' . $number_low_stock . ($number_low_stock == 1 ? ' variation' : ' variations'));
+      } else {
+        echo get_stock_summary_html('green', 'In stock | all variations');
+      }
     }
-		else{
-		$stock = $product->get_stock_quantity();
-			if($stock>0 and  $stock<4){
-				echo '<div style="color:orange;">Low Stock</div>';
-			}
-			
-			elseif($stock==0){
-				echo '<div style="color:red;">Out Of Stock</div>';
-			}
-			else{
-				echo '<div style="color:green;">In Stock </div>';
+    
+    // TODO: Make sure this works for grouped, bundled, etc products
+    else {
+      $stock = $product->get_stock_quantity();
+      if ($stock > 0 and $stock < $LOW_STOCK_THRESHOLD) {
+        echo get_stock_summary_html('orange', 'Low stock');
+      } elseif ($stock == 0) {
+        echo get_stock_summary_html('red', 'Out of stock');
+      } elseif ($stock < 0) {
+        echo get_stock_summary_html('purple', 'On backorder');
+      } else {
+        echo get_stock_summary_html('green', 'In stock');
+      }
+    }
+  }
+}
 
-			}
-			
-		}
-	}
- 
+function get_stock_detail_html($colour, $message) {
+  return '<div style="display:flex; flex-wrap:nowrap; align-items:center;"><div style="border-radius:50px; width:10px; height:10px; background-color:' . $colour . '; margin-right:5px;"></div>' . $message . '</div>';
+}
+
+function get_stock_summary_html($colour, $message) {
+  return '<div style="color:' . $colour . ';">' . $message . '</div>';
 }
